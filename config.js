62b04b7ac1a8bf3b3a2883d686dae3e9c432d674
dz-one1 <vivavoir150@gmail.com>
2025-12-30 19:59:26 +0100

    Mettre à jour le fichier de configuration du magasin avec le système de points - 30/12/2025 18:59:25

--- config.js
+++ config.js
@@ -504,7 +504,7 @@ const STORE_CONFIG = {
     }
   },
   "POINTS_SYSTEM": {
-    "currentPoints": 482,
+    "currentPoints": 480,
     "remainingDays": 30,
     "lastReset": "2025-12-30T17:33:54.743Z",
     "planType": "gratuit",
@@ -584,7 +584,7 @@ const STORE_CONFIG = {
         "user": "admin"
       }
     ],
-    "lastUpdated": "2025-12-30T17:58:07.060Z"
+    "lastUpdated": "2025-12-30T17:59:25.204Z"
   },
   "PIXEL_CODES": {
     "facebook": {
